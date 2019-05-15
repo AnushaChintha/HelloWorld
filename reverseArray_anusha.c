73615aebc81d62de286a5f52e7205710bf3181af
AnushaChintha <anusha.chintha00@gmail.com>
2019-05-15 11:46:47 +0530

    reverse of array

---
+++ reverseArray_anusha.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int size, index1,index2;
+int arr1[size], arr2[size];
+scanf("%d",&size);
+for(index1=0;index1<size;index++)
+{
+scanf("%d",&arr[index1]);
+}
+for(index2=size-1, index1=0;index2>0;index2--)
+{
+arr2[index2]=arr1[index1];
+index1++;
+}
+for(index2=0;index2<size;index2++)
+{
+printf("%d",arr2[index2]);
+}
+}
